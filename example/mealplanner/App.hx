f1d37f5dd64659da7c264352dc932a5d679acd23
Jason O'Neil <jason.oneil@gmail.com>
2020-12-28 21:41:30 +0800

    Get one of the example pages to use params
    - "MealPage" now takes a URL parameter
    - I've refactored the router to use pattern matching
    - I'm more convinced I want to replace all this with a macro that generates the code for you

--- example/mealplanner/App.hx
+++ example/mealplanner/App.hx
@@ -16,38 +16,43 @@ function main() {
 class AppRoutes implements Router {
 	public function new() {}
 
-	public function routeToUri<PageAction, PageParams>(page, params) {
-		return switch page {
-			case HomePage:
-				Some("/");
-			case MealPage:
-				Some("meal");
-			case WeeklyPlanPage:
-				Some("weekly-plan");
-			case ShoppingPage:
-				Some("/shopping");
-			default:
-				None;
+	public function routeToUri<PageParams>(page:Page<Dynamic, PageParams, Dynamic>, params:PageParams):Option<String> {
+		// Why use casts here? Haxe forces type compatibility on `enum.equals()`.
+		// And it won't treat our loose `page` type as compatible with the specific types of each page.
+		// I'm hoping we can have a macro solution to generate these routes that is fully type safe and requires less boilerplate.
+		// Why use if/else instead of a switch statement?
+		// Because `page` is an enum value, it wants to use pattern matching rather than simple equality checks.
+		// This prevents us from having `case ${someVariable}`, so I'm using if/else statements instead.
+		if (page.equals(cast HomePage)) {
+			return Some("/");
+		} else if (page.equals(cast MealPage)) {
+			final mealParams:MealParams = cast params;
+			return Some("/meal/${mealParams.mealName}");
+		} else if (page.equals(cast WeeklyPlanPage)) {
+			return Some("/weekly-plan");
+		} else if (page.equals(cast ShoppingPage)) {
+			return Some("/shopping");
 		}
+		return None;
 	}
 
-	public function uriToRoute<PageParams>(uri:String) {
-		page:Page<Dynamic, Dynamic, Dynamic>, params:Dynamic
-	} {
-
+	public function uriToRoute(uri:String):Option<{
+		page:Page<Dynamic, Dynamic, Dynamic>,
+		params:Dynamic
+	}> {
 		final path = uri.split("?")[0];
 		final parts = path.split("/").filter(s -> s != "");
-		return switch parts {
+		switch parts {
 			case []:
-				Some({page: HomePage, params: {}});
-			case ["meal"]:
-				Some({page: MealPage, params: {}});
+				return Some({page: HomePage, params: {}});
+			case ["meal", mealName]:
+				return Some({page: MealPage, params: {mealName: mealName}});
 			case ["weekly-plan"]:
-				Some({page: WeeklyPlanPage, params: {}});
+				return Some({page: WeeklyPlanPage, params: {}});
 			case ["shopping"]:
-				Some({page: ShoppingPage, params: {}});
+				return Some({page: ShoppingPage, params: {}});
 			default:
-				None;
+				return None;
 		}
 	}
 }
--- example/mealplanner/pages/MealPage.hx
+++ example/mealplanner/pages/MealPage.hx
@@ -4,22 +4,31 @@ import smalluniverse.SmallUniverse;
 import smalluniverse.DOM;
 
 final MealPage = Page(new MealView(), new MealApi());
-typedef MealParams = {}
-typedef MealData = {}
+
+typedef MealParams = {
+	mealName:String
+}
+
+typedef MealData = {
+	mealName:String
+}
 
 class MealView implements PageView<AppAction, MealData> {
 	public function new() {}
 
 	public function render(data:MealData) {
-		return h1([], [text("My meal recipe!")]);
+		return h1([], [text('Recipe for ${data.mealName}')]);
 	}
 }
 
 class MealApi implements PageApi<AppAction, MealParams, MealData> {
 	public function new() {}
 
-	public function getPageData(params:MealParams) {
-		return {}
+	public function getPageData(params:MealParams):MealData {
+		function capitalize(word:String)
+			return word.charAt(0).toUpperCase() + word.substr(1);
+		final mealName = params.mealName.split("-").map(capitalize).join(" ");
+		return {mealName: mealName}
 	}
 
 	public function pageDataShouldUpdate(params:MealParams, action:AppAction) {
