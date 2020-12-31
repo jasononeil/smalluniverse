e215273a5d6e7fa55080032281b326044533efa5
Jason O'Neil <jason.oneil@gmail.com>
2020-12-31 10:19:11 +0800

    Start using source-sans-variable

--- example/mealplanner/ui/Layout.hx
+++ example/mealplanner/ui/Layout.hx
@@ -4,8 +4,11 @@ import smalluniverse.SmallUniverse;
 import smalluniverse.DOM;
 import mealplanner.AppAction;
 
+final sourceSansVariableUrl = "https://cdn.jsdelivr.net/npm/source-sans-pro@3.6.0/source-sans-variable.css";
+
 function Layout(headerContent:Html<AppAction>, mainContent:Html<AppAction>):Html<AppAction> {
 	return [
+		link([rel("stylesheet"), href(sourceSansVariableUrl)]),
 		css(CompileTime.readFile("node_modules/normalize.css/normalize.css")),
 		div([className("container")], [header([], headerContent), main([], mainContent)])
 	];
