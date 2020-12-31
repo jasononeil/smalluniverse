9c989492666bb8521e4ebc5c7f252596bf6ea706
Jason O'Neil <jason.oneil@gmail.com>
2020-12-31 10:06:35 +0800

    Add CSS normalize to the example layout
    (and use yarn workspaces to keep the example NPM dependencies separate from the package)

--- example/mealplanner/ui/Layout.hx
+++ example/mealplanner/ui/Layout.hx
@@ -4,6 +4,9 @@ import smalluniverse.SmallUniverse;
 import smalluniverse.DOM;
 import mealplanner.AppAction;
 
-function Layout(headerContent:Html<AppAction>, mainContent:Html<AppAction>) {
-	return div([className("container")], [header([], headerContent), main([], mainContent)]);
+function Layout(headerContent:Html<AppAction>, mainContent:Html<AppAction>):Html<AppAction> {
+	return [
+		css(CompileTime.readFile("node_modules/normalize.css/normalize.css")),
+		div([className("container")], [header([], headerContent), main([], mainContent)])
+	];
 }
