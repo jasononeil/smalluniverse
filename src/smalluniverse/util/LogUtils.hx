29ac6775b5a5319a840669098b1b310b590d3ca5
Jason O'Neil <jason.oneil@gmail.com>
2022-06-17 08:16:13 +0800

    Make getClassName work for both instances and classes

--- src/smalluniverse/util/LogUtils.hx
+++ src/smalluniverse/util/LogUtils.hx
@@ -3,9 +3,18 @@ package smalluniverse.util;
 using tink.CoreApi;
 
 function getClassName(value:Any, ?pos:haxe.PosInfos):String {
-	final cls = @:nullSafety(Off) Type.getClass(value);
-	if (cls == null) {
-		throw new Error('Could not find class of $value', pos);
+	switch Type.typeof(value) {
+		case TClass(cls):
+			return Type.getClassName(cls);
+		case TObject:
+			final cls = @:nullSafety(Off) Type.getClass(value);
+			if (cls == null) {
+				throw new Error('Could not find class of $value', pos);
+			}
+			return Type.getClassName(cls);
+		case unexpectedType:
+			throw new Error(
+				'Expected an object or a class, but it was ${unexpectedType}'
+			);
 	}
-	return Type.getClassName(cls);
 }
